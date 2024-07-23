2d1fb49263573db8bca398a56feee6778eb99872
Abel Quezada <abelqh9@gmail.com>
2024-07-22 23:14:57 -0500

    Revert "add code from the other debug"
    This reverts commit 67b35373d2cbc6d41646c09b391aeefc0981f95f.

--- app.js
+++ app.js
@@ -14,12 +14,7 @@
 
 const express = require("express");
 
-const {
-  redisClient,
-  getRoomFromCache,
-  addMessageToCache,
-  subClient,
-} = require("./redis");
+const { redisClient, getRoomFromCache, addMessageToCache } = require("./redis");
 const { addUser, getUser, deleteUser } = require("./users");
 
 const app = express();
@@ -53,7 +48,7 @@ const io = require("socket.io")(server);
 // [START cloudrun_websockets_redis_adapter]
 const { createAdapter } = require("@socket.io/redis-adapter");
 // Replace in-memory adapter with Redis
-
+const subClient = redisClient.duplicate();
 io.adapter(createAdapter(redisClient, subClient));
 // [END cloudrun_websockets_redis_adapter]
 // Add error handlers
